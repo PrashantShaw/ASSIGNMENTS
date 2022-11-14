af4fa421b952218c5d1a3546e6112d07bc10dee3
PrashantShaw <prashant.shaw.3154@gmail.com>
2022-11-14 23:34:31 +0530

    Assignment 07

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser");
+const studentRoute = require('./routes/route')
+const app = express()
+const port = 8080
+
+
+// -------------DATABASE------------------------
+mongoose.connect('mongodb://localhost/School', () => {
+    console.log('connected to database')
+})
+
+// --------------MIDDLEWARES--------------------
+app.use(express.urlencoded({ extended: false }));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// your code goes here
+app.use('/api', studentRoute)
+
+
+// ----------------SERVER-----------------------
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app;   
---
+++ src/routes/route.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const router = express.Router()
+const studentArray = require('../InitialData')
+const students = require('../models/studModel')
+const fs = require('fs')
+const path = require('path')
+
+// -------------------------------------------------------
+router.get('/student', async (req, res) => {
+    try {
+        const prevData = await students.find()
+        !prevData.length ?
+            await students.create(studentArray, { _id: 0 })
+            : null
+
+        res.json(await students.find({}, { _id: 0 }).sort({ id: 1 }))
+    }
+    catch (e) { res.json({ message: e.message }) }
+})
+
+// -------------------------------------------------------
+
+router.get('/student/:id', async (req, res) => {
+    try {
+        const searchedStud = await students.findOne(
+            { id: req.params.id }, { _id: 0 })
+
+        searchedStud ?
+            res.json(searchedStud)
+            : res.status(404).json({ message: 'Id is invalid' })
+    }
+    catch (e) { res.json({ message: e.message }) }
+})
+
+// -------------------------------------------------------
+
+router.post('/student', async (req, res) => {
+    try {
+        const { name, currentClass, division } = req.body
+        if (name && currentClass && division) {
+            const filePath = path.join(__dirname, 'nextId.txt')
+            let currId = fs.readFileSync(filePath)
+            await students.create({
+                id: ++currId,
+                ...req.body
+            })
+            fs.writeFileSync(filePath, String(currId))
+            return res.json({ id: currId })
+        }
+        res.status(400).json({ Message: 'Incomplete field(s)' })
+    }
+    catch (e) { res.json({ message: e.message }) }
+})
+
+// -------------------------------------------------------
+
+router.put('/student/:id', async (req, res) => {
+    try {
+        const searchedStudent = await students.findOne({ id: req.params.id })
+        if (searchedStudent) {
+            await students.updateOne(
+                { id: req.params.id }, { $set: req.body }
+            )
+            return res.json(await students.findOne(
+                { id: req.params.id }, { _id: 0 }))
+        }
+        res.status(400).json({ message: "Invalid Id" })
+    }
+    catch (e) { res.json({ message: e.message }) }
+})
+
+// -------------------------------------------------------
+
+router.delete('/student/:id', async (req, res) => {
+    try {
+        const searchedStud = await students.findOne({ id: req.params.id })
+
+        searchedStud ?
+            res.json(await students.deleteOne({ id: req.params.id }))
+            : res.status(404).json({ message: 'Id is invalid' })
+    }
+    catch (e) { res.json({ message: e.message }) }
+})
+
+
+module.exports = router
